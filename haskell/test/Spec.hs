5f6ff1efdf7a89450fec7b9b0c5ebc09bcb6c340
Sean Evans <sean.w.evans@gmail.com>
2025-06-30 10:10:49 -0400

    Add Bool, Pair, and List type variants

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -9,3 +9,9 @@ main = hspec $ do
       matches (V TInt n) TInt
     it "returns False for mismatched Type" $ property $ \n ->
       not (matches (V TInt n) TString)
+    it "matches Bool" $ property $ \b ->
+      matches (V TBool b) TBool
+    it "matches Pair" $ property $ \a b ->
+      matches (V (TPair TInt TString) (a,b)) (TPair TInt TString)
+    it "matches List" $ property $ \xs ->
+      matches (V (TList TInt) xs) (TList TInt)
